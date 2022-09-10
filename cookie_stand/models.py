d8d6f98e6e006049a3c64eb5720974b17991d052
BrendonLH <Brendonhampton1988@gmail.com>
2022-09-10 16:10:28 -0700

    added the required fields to cookie stand

--- cookie_stand/models.py
+++ cookie_stand/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 class Cookie(models.Model):
     name = models.CharField(max_length=256)
+    location = models.CharField(max_length=256, default='')
     baker = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
     ingredients = models.TextField(default="", null=True, blank=True)
+    hourly_sales = models.JSONField(default=list, null=True)
+    minimum_customers_per_hour = models.IntegerField(default=0)
+    maximum_customers_per_hour = models.IntegerField(default=0)
+    average_cookies_per_sale = models.FloatField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.location
